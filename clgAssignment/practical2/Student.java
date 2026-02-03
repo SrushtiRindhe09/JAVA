154eb45e3229ed1f074a64150d0d4c3f70ce8ade
SrushtiRindhe09 <rindhesrushti2006@gmail.com>
2026-02-03 20:38:36 +0530

    moved files in folder

--- clgAssignment/practical2/Student.java
+++ clgAssignment/practical2/Student.java
@@ -1,5 +1,7 @@
-public class Student {
-    public static void main() {
+public class Student 
+{
+    public static void main(String[] args) 
+    {
         int[][] data = {{10,20,30}, {40,50,60}, {70,80,90}};
 
         int sum=0;
