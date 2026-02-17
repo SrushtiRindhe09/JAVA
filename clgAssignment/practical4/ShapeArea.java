02bc095516cf4ead564d5623bfbf1c0af47caa91
SrushtiRindhe09 <rindhesrushti2006@gmail.com>
2026-02-17 19:18:27 +0530

    solved 1st question of 4th practical

---
+++ clgAssignment/practical4/ShapeArea.java
@@ -0,0 +1,32 @@
+public class ShapeArea 
+{ 
+    public static void main(String args[])
+    {
+      Area obj = new Area();
+        obj.calculateArea(5);
+        obj.calculateArea(5, 10);
+        obj.calculateArea(7.5);
+    }
+ 
+
+    
+}
+class Area
+{
+  void calculateArea(int a)
+  {
+    System.out.println("Area of Square: "+ a*a);
+  }
+
+  void calculateArea(int l, int b)
+  {
+    System.out.println("Area of Rectangle: "+ l*b);
+  }
+  void calculateArea(double r)
+  {
+    System.out.println("Area of Circle: "+3.14*r*r);
+  }
+
+}
+
+
