2bc3919fb2918f809b1b04903fd71ab79d93b675
SrushtiRindhe09 <rindhesrushti2006@gmail.com>
2026-02-17 20:22:52 +0530

    solved question in 2nd practical

--- clgAssignment/practical2/LA1String.java
+++ clgAssignment/practical2/LA1String.java
@@ -1,13 +1,11 @@
-package practical2;
-
 /*A university portal maintains student names for the purposes of identification, verification, 
 and formatting. Write a program accepting student's full name and performs the following operations:
  Search for a middle name as substring Replace a word or character Convert the name to Title Case */
 
 import java.util.Scanner;
 
-public class LA1String {
-
+public class LA1String 
+{
 
     public static void main(String[] args)
     { 
@@ -20,7 +18,7 @@ public class LA1String {
       System.out.println("Enter middle name to search:");
       String middlename  = sc.nextLine();
 
-      sc.close();
+      
 
       // Search for middle name as substring
       if (fullName.contains(middlename)) {
@@ -29,8 +27,51 @@ public class LA1String {
           System.out.println("Middle name not found");
       }
 
-      }
-
-
+      
     
-}
+      //3. Replace a word or character
+      System.out.println("Enter a word to replace: ");
+      String oldWord = sc.nextLine();
+
+      System.out.println("Enter the new word: ");
+      String newWord = sc.nextLine();
+
+      String replaceName = fullName.replace(oldWord, newWord);
+      System.out.println("Fullname after Replacemnt: "+ replaceName);
+
+       String titleCaseName = toTitleCase(fullName);
+        System.out.println("Title Case Name: " + titleCaseName);
+
+        sc.close();
+    }
+
+
+       public static String toTitleCase(String name)
+    {
+        String[] words = name.toLowerCase().split(" ");
+        String result = "";
+
+        for (String word : words) {
+            result += Character.toUpperCase(word.charAt(0)) 
+                      + word.substring(1) + " ";
+        }
+
+        return result.trim();
+    }
+}  
+
+/*Output:
+
+Enter your full name:
+srushti abasaheb rindhe
+Enter middle name to search:
+abasaheb
+Middle name found in: srushti abasaheb rindhe
+Enter a word to replace:
+srushti
+srushti
+Enter the new word:
+sruu
+Fullname after Replacemntsruu abasaheb rindhe
+Title Case Name: Srushti Abasaheb Rindhe
+*/
