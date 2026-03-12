f8b9712ef9dd95f9a36064d8f78137ca035c0bc2
Srushti Abasaheb Rindhe <155223275+SrushtiRindhe09@users.noreply.github.com>
2026-03-12 23:03:24 +0530

    Fix formatting in LA2Student.java main method

--- clgAssignment/practical1/LA2Student.java
+++ clgAssignment/practical1/LA2Student.java
@@ -1,6 +1,6 @@
 public class Student 
 {
-    public static void main(String[] args) 
+    public static void main(String[] args)
     {
         int[][] data = {{10,20,30}, {40,50,60}, {70,80,90}};
 
